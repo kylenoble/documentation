bfb8d218a2f8b128022ad45a0ad3cf83c558c9ef
kylenoble <kyle.nobl@gmail.com>
2014-10-23 20:31:32 -0400

    fix routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "User created successfully!"
-      redirect_to @user
+      redirect_to "/"
     else
       render 'new'
     end
