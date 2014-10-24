b1230c9dc8890d1c966dd745bb87ff27c061b633
kylenoble <kyle.nobl@gmail.com>
2014-10-23 20:21:58 -0400

    fix secrets

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
     validates :password, length: { minimum: 6 }
   
   def validate_secret(attr_name, secret)
-		validates attr_name, inclusion: { in: secret, message: "The key is incorrect" }
+		validates attr_name, inclusion: { in: %w(secret), message: "The key is incorrect" }
 	end 	
 
   @secret = ENV["SECRET_PASS"]
